f362c0ff61df2d68022287a624f63d5f70590c0d
spott <atte.saarinen@iki.fi>
2014-01-18 01:31:21 +0200

    players and games are now stored to mongodb

--- dotabotti.js
+++ dotabotti.js
@@ -1,7 +1,10 @@
 var irc = require('irc');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/dotabotti');
 
 var config = {
-	channels: [ '#dotabotti_testi' ],
+	channels: [ '#dotabotti' ],
 	server: 'irc.freenode.net',
 	nick: 'Meepo',
 	debug: true
@@ -36,15 +39,26 @@ var game = null;
 var signed = [];
 var picking = null;
 
-function sign(nick)
+function sign(nick, callback)
 {
-	if(game != null && signed.indexOf(nick) < 0 && signed.length < 10 && game.state == gamestate.signup)
+	if(game != null && signed.length < 10 && game.state == gamestate.signup)
 	{
-		signed.push(nick);
-		return true;
+		get_player(nick, true, function(player) {
+			if(signed.indexOf(player) < 0)
+			{
+				signed.push(player);
+				callback(player);
+			}
+			else 
+			{
+				callback(null);
+			}
+		});
 	}
-
-	return false;
+	else
+	{
+		callback(null);
+	}
 }
 
 function out(nick)
@@ -66,12 +80,12 @@ function start(nick)
 		game = {
 			radiant: {
 				name: 'Radiant',
-				captain: '',
+				captain: null,
 				players: []
 			},
 			dire: {
 				name: 'Dire',
-				captain: '',
+				captain: null,
 				players : []
 			},
 			gameid: '',
@@ -80,7 +94,16 @@ function start(nick)
 			winner: null
 		}
 
-		signed = [nick];
+		get_player(nick, true, function(player) { signed = [player] });
+
+		get_player('winkzi', true, function(player) { signed.push(player); });
+		get_player('shancial', true, function(player) { signed.push(player); });
+		get_player('agu', true, function(player) { signed.push(player); });
+		get_player('ra', true, function(player) { signed.push(player); });
+		get_player('cadiac', true, function(player) { signed.push(player); });
+		get_player('snowell', true, function(player) { signed.push(player); });
+		get_player('cawa', true, function(player) { signed.push(player); });
+		get_player('aira', true, function(player) { signed.push(player); });
 
 		return true;
 	}
@@ -95,12 +118,12 @@ function challenge(nick)
 		game = {
 			radiant: {
 				name: 'Radiant',
-				captain: nick,
+				captain: null,
 				players: []
 			},
 			dire: {
 				name: 'Dire',
-				captain: '',
+				captain: null,
 				players : []
 			},
 			gameid: '',
@@ -109,6 +132,7 @@ function challenge(nick)
 			winner: null
 		}
 
+		get_player(nick, true, function(player) { game.radiant.captain = player });
 		signed = [nick];
 
 		return true;
@@ -121,7 +145,7 @@ function accept(nick)
 {
 	if(game != null && game.state == gamestate.challenged && game.radiant.captain != nick)
 	{
-		game.dire.captain = nick;
+		get_player(nick, true, function(player) { game.dire.captain = player });
 		game.dire.players = [];
 		signed.push(nick);
 		game.state = gamestate.signup;
@@ -159,6 +183,9 @@ function end(winner)
 		{
 			game.winner = game.dire;
 		}
+
+		var games = db.get('games');
+		games.insert(game, function(err, res) { if(err) console.log(err); });
 
 		return true;
 	}
@@ -225,7 +252,7 @@ function get_signed()
 	var players = '';
 	for(var i = 0; i < signed.length; i++)
 	{
-		players += (signed[i] + ' ');
+		players += (signed[i].nick + ' ');
 	}
 	return players;
 }
@@ -241,7 +268,7 @@ function get_dire()
 	
 	for(var i = 0; i < game.dire.players.length; i++)
 	{
-		dire += (game.dire.players[i] + ' ');
+		dire += (game.dire.players[i].nick + ' ');
 	}
 	
 	return dire;
@@ -258,11 +285,35 @@ function get_radiant()
 	
 	for(var i = 0; i < game.radiant.players.length; i++)
 	{
-		radiant += (game.radiant.players[i] + ' ');
+		radiant += (game.radiant.players[i].nick + ' ');
 	}
 	
 	return radiant;
-}						
+}
+
+function get_player(nick, create, callback) {
+	var players = db.get('players');
+ 	players.findOne({ nick: nick })
+    .on('complete', function (err, res) {
+    	if(err)
+    	{
+    		console.log(err);
+    		callback(null);
+    	}
+
+    	if(res == null && create == true)
+    	{
+    		players.insert({ nick: nick, rating: 1500, wins: 0, losses: 0 }, function(err, res) {
+    			if(err) console.log(err);
+    			callback(res);
+    		});
+    	}
+    	else
+    	{
+    		callback(res);
+    	}
+    });
+}
 
 bot.addListener('message', function(from, to, text, message) {
 	var str = text.split(' ');
@@ -271,6 +322,19 @@ bot.addListener('message', function(from, to, text, message) {
 	{
 		switch(str[0])
 		{
+			case '.stats':
+				get_player(str[1], false, function(player) {
+					if(player != null)
+					{
+						bot.say(to, 'Nick: ' + player.nick + ' Rating: ' + player.rating + ' Wins: ' + player.wins + ' Losses: ' + 
+							player.losses + ' Matches: ' + (player.wins + player.losses));
+					}
+					else
+					{
+						bot.say(to, 'Not found');
+					}
+				});
+				break;
 			case '.signed':
 				bot.say(to, get_signed());
 				break;
@@ -301,30 +365,32 @@ bot.addListener('message', function(from, to, text, message) {
 				}
 				break;
 			case '.sign':
-				if(sign(from))
-				{
-					bot.say(to, from + ' added. ' + signed.length + '/10');
-
-					if(signed.length == 10)
+				sign(from, function(player) {
+					if(player != null)
 					{
-						if(game.mode == gamemode.draft)
+						bot.say(to, from + ' added. ' + signed.length + '/10');
+
+						if(signed.length == 10)
 						{
-							draft();
-							bot.say(to, 'Draft starts. ' + picking.name + 's turn to pick. Captain: ' + picking.captain + '.');
-							bot.say(to, 'Available players: ' + get_signed());
-						}
-						else if(game.mode == gamemode.shuffle)
-						{
-							game.state = gamestate.shuffle;
-							shuffle();
-							bot.say(to, 'Teams shuffled. Radiant: ' + get_radiant() + ' Dire: ' + get_dire() + '. Type .shuffle to reshuffle teams or type .go to proceed to game.');
+							if(game.mode == gamemode.draft)
+							{
+								draft();
+								bot.say(to, 'Draft starts. ' + picking.name + 's turn to pick. Captain: ' + picking.captain + '.');
+								bot.say(to, 'Available players: ' + get_signed());
+							}
+							else if(game.mode == gamemode.shuffle)
+							{
+								game.state = gamestate.shuffle;
+								shuffle();
+								bot.say(to, 'Teams shuffled. Radiant: ' + get_radiant() + ' Dire: ' + get_dire() + '. Type .shuffle to reshuffle teams or type .go to proceed to game.');
+							}
 						}
 					}
-				}
-				else 
-				{
-					bot.say(to, 'Error?! :G');
-				}
+					else 
+					{
+						bot.say(to, 'Error?! :G');
+					}
+				});
 				break;
 			case '.out':
 				if(out(from))
